1cc0c0b4cc89cd34ef82fcb6e7c5a35ab7c3e568
Mojasagwe <78989798+Mojasagwe@users.noreply.github.com>
2025-06-07 10:12:52 +0200

    Initial commit: Setup React project with Tailwind CSS and SkipSelector component

---
+++ src/components/SkipSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SkipSelector = () => {
+  return (
+    <div className="skip-selector bg-neutral-900 min-h-screen text-white p-5">
+      <h1 className="text-3xl font-bold mb-4">Skip Selector Page</h1>
+      <p className="text-lg">This will be our skip selection page</p>
+    </div>
+  );
+};
+
+export default SkipSelector; 
